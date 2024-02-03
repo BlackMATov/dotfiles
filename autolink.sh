94b4f5ff63d8ab0774a4953d3d0e66901fa25a3e
BlackMATov <blackmat@mail.ru>
2024-02-04 02:54:48 +0700

    remove old vim configs

--- autolink.sh
+++ autolink.sh
@@ -2,7 +2,6 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE}" )" && pwd )"
 
-ln -shf "${DIR}/.vimrc" "${HOME}/.vimrc"
 ln -shf "${DIR}/.zshrc" "${HOME}/.zshrc"
 ln -shf "${DIR}/htoprc" "${HOME}/htoprc"
 ln -shf "${DIR}/nvim" "${HOME}/.config/nvim"
