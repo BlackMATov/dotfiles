e4fa6def65dda5d8fc5bb9226f4fa585cd901ba7
BlackMATov <blackmat@mail.ru>
2024-11-25 10:44:49 +0700

    telescope window tuning

--- nvim/lua/blackmatov/plugins/telescope.lua
+++ nvim/lua/blackmatov/plugins/telescope.lua
@@ -4,5 +4,16 @@ if not telescope_status then
     return
 end
 
-telescope.setup()
+telescope.setup {
+    defaults = {
+        layout_config = {
+            width = 0.9,
+            height = 0.9,
+            preview_width = 0.5,
+            prompt_position = "top",
+        },
+        sorting_strategy = "ascending",
+    }
+}
+
 telescope.load_extension("fzf")
