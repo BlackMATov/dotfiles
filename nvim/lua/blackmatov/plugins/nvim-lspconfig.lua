6bfc1585a9c8d4063ba58cf2ab6ac9b54cebe1d4
BlackMATov <blackmat@mail.ru>
2025-10-07 07:18:31 +0700

    replace the deprecated lspconfig

--- nvim/lua/blackmatov/plugins/nvim-lspconfig.lua
+++ nvim/lua/blackmatov/plugins/nvim-lspconfig.lua
@@ -1,9 +1,3 @@
-local lspconfig_status, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status then
-    print("lspconfig not found")
-    return
-end
-
 local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_nvim_lsp_status then
     print("cmp_nvim_lsp not found")
@@ -12,25 +6,29 @@ end
 
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
-if lspconfig.lua_ls then
-    lspconfig.lua_ls.setup {
-        capabilities = capabilities,
-        settings = {
-            Lua = {
-                completion = {
-                    autoRequire = false,
-                },
-                runtime = {
-                    pathStrict = true,
-                    version = "LuaJIT",
-                }
-            }
-        },
-    }
-end
+local vim = _G["vim"]
 
-if lspconfig.jsonls then
-    lspconfig.jsonls.setup {
-        capabilities = capabilities,
-    }
-end
+-- lua_ls
+
+vim.lsp.config('lua_ls', {
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            completion = {
+                autoRequire = false,
+            },
+            runtime = {
+                pathStrict = true,
+                version = "LuaJIT",
+            }
+        }
+    },
+})
+vim.lsp.enable('lua_ls')
+
+-- jsonls
+
+vim.lsp.config('jsonls', {
+    capabilities = capabilities,
+})
+vim.lsp.enable('jsonls')
